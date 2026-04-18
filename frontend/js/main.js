b6f49743b02b9a3faf153fa35fa114105f2365a2
angelmanrique1512-del <angelmanrique1512@gmail.com>
2026-04-18 16:43:33 -0500

    Update main.js

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,38 +1,34 @@
-const API = "http://localhost:8080/asistencia";
+function cargarDocentes() {
 
-function cargarAsistencia() {
-  fetch(API)
+  // ocultar historial
+  document.getElementById("historial").style.display = "none";
+
+  // mostrar docentes
+  document.getElementById("lista-docentes").style.display = "block";
+
+  fetch("http://localhost:8080/docentes")
     .then(res => res.json())
     .then(data => {
-      const tabla = document.getElementById("tabla-asistencia");
+
+      let tabla = document.getElementById("tabla-docentes");
       tabla.innerHTML = "";
-      data.forEach(a => {
+
+      data.forEach(d => {
         tabla.innerHTML += `
           <tr>
-            <td>${a.dni}</td>
-            <td>${a.nombre} ${a.apellidos || ""}</td>
-            <td>${a.curso}</td>
-            <td>${a.fecha}</td>
-            <td>${a.hora_entrada || "-"}</td>
-            <td>${a.estado || "Pendiente"}</td>
+            <td>${d.dni}</td>
+            <td>${d.nombre}</td>
+            <td>${d.apellidos}</td>
+            <td>${d.curso}</td>
           </tr>
         `;
       });
+
     });
 }
+function mostrarHistorial() {
 
-function registrar() {
-  const dni = document.getElementById("dni").value;
-  fetch(API + "/registrar", {
-    method: "POST",
-    headers: {"Content-Type": "application/json"},
-    body: JSON.stringify({ dni })
-  })
-  .then(res => res.json())
-  .then(data => {
-    document.getElementById("mensaje").innerText = data.mensaje;
-    cargarAsistencia();
-  });
+  document.getElementById("historial").style.display = "block";
+  document.getElementById("lista-docentes").style.display = "none";
+
 }
-
-document.addEventListener("DOMContentLoaded", cargarAsistencia);
